1cd240b628d4ddcd0493b536275197851647a919
Eduardo <eduardonaves02@outlook.com>
2023-01-19 15:55:49 -0300

    finalização pagina de login

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import {BsFacebook,BsApple} from 'react-icons/bs'
+import {FcGoogle} from 'react-icons/fc'
+import {TbFaceId} from 'react-icons/tb'
 
+
+import img_login from '../../img/beach_login.svg'
 import './Login.sass'
+
+
 
 const Login = () => {
   return (
-    <div>
-        <a href="#">Joao Alfredo</a>
+    <div className='container_login'>
+      <div className="img_login">
+        <img src={img_login} alt="alt" />
+      </div>
+      <div className="user_login">
+        <h2>Só mais um pouquinho <br/> e você ja pode pedir!</h2>
+        <p>Como deseja fazer o login?</p>
+        <div className="social">
+          <button className='apple'><b><BsApple/></b> Entrar com a Apple</button>
+          <button className='google'><b><FcGoogle/></b> Entrar com o Google</button>
+          <button className='facebook'><b><BsFacebook/></b> Entrar com Facebook</button>
+        </div>
+        <div className="login">
+          <button>Celular</button>
+          <button>E-mail</button>
+          <button>Reconhecimento Facial <TbFaceId/></button>
+        </div>
+      </div>
     </div>
   )
 }
